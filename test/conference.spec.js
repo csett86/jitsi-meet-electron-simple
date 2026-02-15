e93c02ed7244d524f45fa7de6b45f75450903caa
Christoph Settgast <csett86_git@quicksands.de>
2026-02-15 22:56:12 +0100

    randomize room name

--- test/conference.spec.js
+++ test/conference.spec.js
@@ -32,7 +32,8 @@ test.describe('Jitsi Meet Conference Loading', () => {
 
   test('should load conference from URL and create iframe via API', async () => {
     // Enter a semi-random conference URL
-    const conferenceUrl = 'https://alpha.jitsi.net/ztexfftt644';
+    const roomName = Math.random().toString(36).replace(/[0-9]/g, '').substring(1);
+    const conferenceUrl = 'https://alpha.jitsi.net/'.concat(roomName);
     await window.locator('#jitsi-url').fill(conferenceUrl);
 
     // Click the Go button
@@ -44,6 +45,6 @@ test.describe('Jitsi Meet Conference Loading', () => {
     
     // Verify the iframe is present and the title of the conference is visible on prejoin
     await expect(iframe).toBeAttached();
-    await expect(iframe.contentFrame().getByText('Ztexfftt 644')).toBeVisible();
+    await expect(iframe.contentFrame().getByText(roomName)).toBeVisible();
   });
 });
