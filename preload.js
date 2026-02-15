9b82762e6b63fecbd5a6c235cb6403d5771dc4b4
Christoph Settgast <csett86_git@quicksands.de>
2026-02-15 10:03:55 +0100

    fixup

--- preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge } = require('electron');
-const { setupScreenSharingRender } = require('@jitsi/electron-sdk');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-    setupScreenSharing: (api) => setupScreenSharingRender(api)
-});
